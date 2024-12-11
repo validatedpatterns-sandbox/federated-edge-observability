b217ea800573e1c27170ce5dc075b9cfd1af7f74
Martin Jackson <martjack@redhat.com>
2024-12-11 08:26:12 -0600

    Improve certpair creation script

--- scripts/make_snakeoil_certpair.sh
+++ scripts/make_snakeoil_certpair.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-pattern=$1
-name=$2
-domain=$3
+name=$1
 
-openssl req -x509 -newkey rsa:4096 -sha256 -days 365 \
-  -nodes -keyout "~/$pattern-$name.key" -out "~/$pattern-$name.crt" -subj "/CN=$pattern-$name" \
-  -addext "subjectAltName=DNS:$name.$domain"
+pattern=$( ./common/scripts/determine-pattern-name.sh )
+domain=$( oc get ingresses.config/cluster -o jsonpath={.spec.domain} )
+
+certname="$pattern-$name"
+cert_path="$HOME/$pattern-$name.crt"
+key_path="$HOME/$pattern-$name.key"
+
+if [[ -f "$cert_path" && -f "$key_path" ]]; then
+    echo "Snakeoil certpair $certname already exists, not re-creating"
+else
+    openssl req -x509 -newkey rsa:4096 -sha256 -days 365 \
+        -nodes -keyout "$key_path" -out "$cert_path" -subj "/CN=$certname" \
+        -addext "subjectAltName=DNS:$name.$domain"
+fi
+
+echo "Key Path: $key_path"
+echo "Cert Path: $cert_path"
